43bd1eba20a9889bfd8dd0103de174826da9baaf
Thales Henrique <thaales_jp@hotmail.com>
2020-07-16 13:18:14 -0300

    First commit

---
+++ src/main/java/com/thaleshenrique/mapfre/MapfreApplication.java
@@ -0,0 +1,13 @@
+package com.thaleshenrique.mapfre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapfreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapfreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thaleshenrique/mapfre/MapfreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaleshenrique.mapfre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapfreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
