25f922c6c253a8da88417f26ca2a97efd72cfab7
Thales Henrique <thaales_jp@hotmail.com>
2020-07-16 13:34:16 -0300

    Verificando se API Rest está funcionando

---
+++ src/main/java/com/thaleshenrique/mapfre/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.thaleshenrique.mapfre.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() 
+	{
+		return "Rest está funcionando";
+	}
+
+}
