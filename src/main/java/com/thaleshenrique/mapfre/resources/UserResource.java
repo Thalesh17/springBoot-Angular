a134e324567362ef2ee42a1b2fb28270a95bc563
Thales Henrique <thaales_jp@hotmail.com>
2020-07-16 13:52:45 -0300

    Criando banco de dados H2

--- src/main/java/com/thaleshenrique/mapfre/resources/UserResource.java
+++ src/main/java/com/thaleshenrique/mapfre/resources/UserResource.java
@@ -1,17 +1,29 @@
 package com.thaleshenrique.mapfre.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thaleshenrique.mapfre.domain.User;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() 
+	public List<User> getAll() 
 	{
-		return "Rest está funcionando";
+		User user = new User(1, "Thales");
+		User user2 = new User(2, "Henrique");
+		
+		List<User> users = new ArrayList<User>();
+		users.add(user);
+		users.add(user2);
+		
+		return users;
 	}
 
 }
